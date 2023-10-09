d5df3c646da0365964a75b886eb07555a71e0c5f
Alona Didyk <alionadiduk31@gmail.com>
2023-10-09 16:31:51 +0300

    lesson-2-commit

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,26 @@
-let js = "amazing";
+// let js = "amazing";
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "jonas";
-console.log(firstName);
+// let firstName = "jonas";
+// console.log(firstName);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+// console.log(typeof true);
+console.log(typeof jsIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+jsIsFun = "yes";
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //object(bug in js)
